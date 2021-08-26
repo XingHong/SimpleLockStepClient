5ffae49448db858ee421927e364dd4749e3fb5c9
XingHong <xinghonggz@gmail.com>
2021-08-26 18:13:31 +0800

    add Launcher

---
+++ Assets/Scripts/Logic/Framework/Launcher.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Launcher : ILifeCycle
+{
+    private ServiceContainer serviceContainer;
+    private ManagerContainer mgrContainer;
+    private IEventRegisterService eventService;
+
+    public static Launcher Instance { get; private set; }
+
+    public void DoAwake(IServiceContainer container)
+    {
+        if (Instance != null)
+        {
+            return;
+        }
+        Instance = this;
+        serviceContainer = container as ServiceContainer;
+        mgrContainer = new ManagerContainer();
+        eventService = new EventRegisterService();
+
+        var svcs = serviceContainer.GetAllServices();
+        foreach (var service in svcs)
+        {
+            if (service is BaseService baseService)
+            {
+                mgrContainer.RegisterManager(baseService);
+            }
+        }
+
+        serviceContainer.RegisterService(eventService);
+    }
+
+    public void DoStart()
+    {
+        foreach (var mgr in mgrContainer.AllMgrs)
+        {
+            mgr.InitReference(serviceContainer);
+        }
+
+        foreach (var mgr in mgrContainer.AllMgrs)
+        {
+            eventService.RegisterEvent<EEvent, GlobalEventHandler>("OnEvent", EventHelper.AddListener, mgr);
+        }
+
+        foreach (var mgr in mgrContainer.AllMgrs)
+        {
+            mgr.DoAwake(serviceContainer);
+        }
+
+        foreach (var mgr in mgrContainer.AllMgrs)
+        {
+            mgr.DoStart();
+        }
+    }
+
+    public void DoUpdate(float deltaTime)
+    { 
+    }
+
+    public void DoDestroy()
+    {
+        
+    }
+
+    public void OnApplicationQuit()
+    {
+        DoDestroy();
+    }
+}
---
+++ Assets/Scripts/View/LogicView/Framework/MainScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    private ServiceContainer serviceContainer;
+
+    public Launcher GameLauncher = new Launcher();
+    void Awake()
+    {
+        serviceContainer = new BaseGameServicesContainer();
+
+        GameLauncher.DoAwake(serviceContainer);
+    }
+
+    void Start()
+    {
+        GameLauncher.DoStart();
+    }
+
+    void Update()
+    {
+        GameLauncher.DoUpdate(Time.deltaTime);
+    }
+
+    void OnDestroy()
+    {
+        GameLauncher.DoDestroy();
+    }
+
+    void OnApplicationQuit()
+    {
+        GameLauncher.OnApplicationQuit();
+    }
+}
