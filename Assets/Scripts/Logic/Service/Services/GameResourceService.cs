6525532343095b60ef108cadad45a76359cc4d42
XingHong <xinghonggz@gmail.com>
2021-11-23 18:12:26 +0800

    add base entity view

---
+++ Assets/Scripts/Logic/Service/Services/GameResourceService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+public class GameResourceService : BaseGameService, IGameResourceService
+{
+    public string pathPrefix = "Prefabs/";
+    private Dictionary<int, GameObject> id2Prefab = new Dictionary<int, GameObject>();
+
+    public object LoadPrefab(int id)
+    {
+        if (id2Prefab.TryGetValue(id, out var val))
+        {
+            return val;
+        }
+        var prefab = (GameObject)Resources.Load(pathPrefix + "Tower");
+        id2Prefab[id] = prefab;
+        return prefab;
+    }
+}
---
+++ Assets/Scripts/View/LogicView/Entity/BaseEntityView.cs
@@ -0,0 +1,33 @@
+﻿using Lockstep.Math;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEntityView : MonoBehaviour, IEntityView
+{
+    public BaseEntity baseEntity;
+
+    public void BindEntity(BaseEntity e, BaseEntity oldEntity = null)
+    {
+        e.EntityView = this;
+        this.baseEntity = e;
+        var updateEntity = oldEntity ?? e;
+        transform.position = updateEntity.transform.Pos3.ToVector3();
+        transform.rotation = Quaternion.Euler(0, updateEntity.transform.deg.ToFloat(), 0);
+    }
+
+    public void OnDead()
+    {
+        GameObject.Destroy(gameObject);
+    }
+
+    public void OnRollbackDestroy()
+    {
+        GameObject.Destroy(gameObject);
+    }
+
+    public void OnTakeDamage(int amount, LVector3 hitPoint)
+    {
+        //throw new System.NotImplementedException();
+    }
+}
